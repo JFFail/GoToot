1c2d0e82f60bb40bfe2adcb9ccc42b3c2b153205
JFFail <jjfabry@protonmail.com>
2020-07-03 13:25:46 +0000

    Added function to validate the authorization token.

--- goToot.go
+++ goToot.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	//"net/http"
+	"net/http"
 	"os"
 )
 
@@ -12,6 +12,45 @@ import (
 type ClientConfig struct {
 	Token    string `json:"access_token"`
 	Instance string `json:"instance"`
+}
+
+func verifyToken(bearer string, url string) bool {
+	// Append the endpoint to the URL.
+	fullUrl := fmt.Sprintf("%v/apps/verify_credentials", url)
+
+	// Create an HTTP client.
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(3)
+	}
+
+	// Set the header.
+	request.Header.Set("Authorization", bearer)
+
+	// Make the request.
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(4)
+	}
+
+	defer response.Body.Close()
+
+	// Read the data.
+	respData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(5)
+	}
+
+	// Ensure it's not null.
+	if respData != nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 // Main function.
@@ -32,7 +71,15 @@ func main() {
 	}
 
 	// Create the base URL for our instance.
-	baseURL := configInfo.Instance + "/api/v1/"
+	baseURL := fmt.Sprintf("%v/api/v1", configInfo.Instance)
 
-	fmt.Println(baseURL)
+	// Create the header we'll use for authorization.
+	bearerHeader := fmt.Sprintf("Bearer %v", configInfo.Token)
+
+	// Verify the token is valid.
+	if verifyToken(bearerHeader, baseURL) {
+		fmt.Println("Token is valid!")
+	} else {
+		fmt.Println("Bad token!")
+	}
 }
