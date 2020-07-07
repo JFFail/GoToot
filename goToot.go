fe755cd2d936761fcaa5c9d97d8271c119c0f8dc
JFFail <jjfabry@protonmail.com>
2020-07-07 01:02:35 +0000

    Removed the MultiToot struct from the code entirely and replaced it by using slices of the SingleToot struct. It made no sense to have a dedicated slice struct duplicate.

--- goToot.go
+++ goToot.go
@@ -120,67 +120,7 @@ type SingleToot struct {
 	Poll             interface{}   `json:"poll"`
 }
 
-// Struct for multiple toot responses from timelines.
-type MultiToot []struct {
-	ID                 string      `json:"id"`
-	CreatedAt          time.Time   `json:"created_at"`
-	InReplyToID        interface{} `json:"in_reply_to_id"`
-	InReplyToAccountID interface{} `json:"in_reply_to_account_id"`
-	Sensitive          bool        `json:"sensitive"`
-	SpoilerText        string      `json:"spoiler_text"`
-	Visibility         string      `json:"visibility"`
-	Language           string      `json:"language"`
-	URI                string      `json:"uri"`
-	URL                string      `json:"url"`
-	RepliesCount       int         `json:"replies_count"`
-	ReblogsCount       int         `json:"reblogs_count"`
-	FavouritesCount    int         `json:"favourites_count"`
-	Favourited         bool        `json:"favourited"`
-	Reblogged          bool        `json:"reblogged"`
-	Muted              bool        `json:"muted"`
-	Bookmarked         bool        `json:"bookmarked"`
-	Pinned             bool        `json:"pinned"`
-	Content            string      `json:"content"`
-	Reblog             interface{} `json:"reblog"`
-	Application        struct {
-		Name    string `json:"name"`
-		Website string `json:"website"`
-	} `json:"application"`
-	Account struct {
-		ID             string        `json:"id"`
-		Username       string        `json:"username"`
-		Acct           string        `json:"acct"`
-		DisplayName    string        `json:"display_name"`
-		Locked         bool          `json:"locked"`
-		Bot            bool          `json:"bot"`
-		Discoverable   bool          `json:"discoverable"`
-		Group          bool          `json:"group"`
-		CreatedAt      time.Time     `json:"created_at"`
-		Note           string        `json:"note"`
-		URL            string        `json:"url"`
-		Avatar         string        `json:"avatar"`
-		AvatarStatic   string        `json:"avatar_static"`
-		Header         string        `json:"header"`
-		HeaderStatic   string        `json:"header_static"`
-		FollowersCount int           `json:"followers_count"`
-		FollowingCount int           `json:"following_count"`
-		StatusesCount  int           `json:"statuses_count"`
-		LastStatusAt   string        `json:"last_status_at"`
-		Emojis         []interface{} `json:"emojis"`
-		Fields         []struct {
-			Name       string    `json:"name"`
-			Value      string    `json:"value"`
-			VerifiedAt time.Time `json:"verified_at"`
-		} `json:"fields"`
-	} `json:"account"`
-	MediaAttachments []interface{} `json:"media_attachments"`
-	Mentions         []interface{} `json:"mentions"`
-	Tags             []interface{} `json:"tags"`
-	Emojis           []interface{} `json:"emojis"`
-	Card             interface{}   `json:"card"`
-	Poll             interface{}   `json:"poll"`
-}
-
+// Function to query data from Mastodon.
 func queryMasto(bearer string, url string) []byte {
 	// Create an HTTP client
 	client := &http.Client{}
@@ -336,7 +276,7 @@ func getTootContent() string {
 }
 
 // Function to print the toots in a timeline.
-func printToots(allToots MultiToot) {
+func printToots(allToots []SingleToot) {
 	var tootContent string
 	// Loop through the slice backwards.
 	for i := len(allToots) - 1; i >= 0; i-- {
@@ -390,7 +330,7 @@ func main() {
 	var cwText string
 	var currentPost string
 	var currentTimeline []byte
-	var currentTLParsed MultiToot
+	var currentTLParsed []SingleToot
 	userPrompt := fmt.Sprintf("[%v]: ", currentUser.Acct)
 	reader := bufio.NewReader(os.Stdin)
 	for userChoice != "quit" {
